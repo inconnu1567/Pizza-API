8fe245eefbc58b0d7e9ef90de4a7cf0c2a2581e1
inconnu1567 <you@example.com>
2022-05-24 21:03:19 +0200

    aaa

---
+++ terminal/Order.py
@@ -0,0 +1,11 @@
+{
+    "order":{
+        "order_id":0,
+        "ordered_at":"2012-04-23T18:05:32.511Z",
+        "status":"In Progress",
+    },
+
+    "ordered_at":"2012-04-23T18:05:32.511Z",
+    "delivery_time":"2012-04-23T18:25:43.511Z",
+
+}
